4665510543d42bc1383ceb1fa29991e680adee53
Jonathan Zhang <zhangjianan1996@qq.com>
2017-09-19 15:45:05 +0800

    update

--- CppLearn/test/housework/09-19/01/main.cpp
+++ CppLearn/test/housework/09-19/01/main.cpp
@@ -6,10 +6,25 @@
 using namespace std;
 
 long init_num(int n){
-
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return n*init_num(n-1);
+    }
 }
 
-int main(){
-    int count
-    cin >>
+void main(){
+    int counts,i;
+	long sum = 0;
+    cout << "请输入偶数！" << endl;
+    cin >> counts;
+    if(counts % 2 == 0){
+        for (i = 2; i <= counts; i=i+2) {
+            sum = sum + init_num(i);
+        }
+		cout << sum << endl;
+    }else{
+        cout << "输入有误，请输入偶数";
+        exit(-1);
+    }
 }
