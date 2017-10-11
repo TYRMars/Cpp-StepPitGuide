959c37023d6c32b0628867dde938280e33848656
Jonathan Zhang <zhangjianan1996@qq.com>
2017-10-11 23:26:58 +0800

    update

--- CppLearn/test/housework/09-20/02/main.cpp
+++ CppLearn/test/housework/09-20/02/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include "ThingsInput.h"
 
 using namespace std;
@@ -42,19 +41,30 @@ void ThingsCreate::InputIn()
 
 void ThingsCreate::OutputIn()
 {
-    ofstream outfile("equi.txt",ofstream::app);
-    int j=1;
-    while (j==1){
-        ThingsCreate::InputIn();
-        outfile << sysID << " " << sysName << " " << sysClass << endl;
-        cout << "是否继续输入(输入1继续)";
-        cin >> j;
+    if(fstream("equi.txt")){
+        ofstream outfile("equi.txt",ofstream::app);
+        int j=1;
+        while (j==1){
+            ThingsCreate::InputIn();
+            outfile << sysID << " " << sysName << " " << sysClass << endl;
+            cout << "是否继续输入(输入1继续),输入其他任意结束";
+            cin >> j;
+        }
+    }else{
+        ofstream out("equi.txt");
+        int j=1;
+        while (j==1){
+            ThingsCreate::InputIn();
+            out << sysID << " " << sysName << " " << sysClass << endl;
+            cout << "是否继续输入(输入1继续),输入其他任意结束";
+            cin >> j;
+        }
     }
 }
 
 void ThingsCreate::OutputOut()
 {
-    int i =0,numa=0,numb=0,numc=0;
+    int i =0,num_a=0,num_b=0,num_c=0;
     ifstream in("equi.txt");
     string s[200];
 
@@ -67,22 +77,22 @@ void ThingsCreate::OutputOut()
         cout<<s[i]<<endl;
         if(strstr(s[i].c_str(), a) != NULL)
         {
-            numa++;
+            num_a++;
         }
         if(strstr(s[i].c_str(), b) != NULL)
         {
-            numb++;
+            num_b++;
         }
         if(strstr(s[i].c_str(), c) != NULL)
         {
-            numc++;
+            num_c++;
         }
         i++;
     }
     in.close();
-    cout<<"实验设备"<<numa<<endl;
-    cout<<"办公设备"<<numb<<endl;
-    cout<<"教学设备"<<numc<<endl;
+    cout<<"实验设备"<<num_a<<endl;
+    cout<<"办公设备"<<num_b<<endl;
+    cout<<"教学设备"<<num_c<<endl;
 }
 
 int main() {
@@ -97,5 +107,6 @@ int main() {
     else if (i==2)
     {
         t.OutputOut();
+        return 0;
     }
 }
