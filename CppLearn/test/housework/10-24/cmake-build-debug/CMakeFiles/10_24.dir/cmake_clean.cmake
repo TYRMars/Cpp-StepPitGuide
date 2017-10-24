091abf4ff70cf5f6a2849ff88d17a51ba45067a9
Jonathan Zhang <zhangjianan1996@qq.com>
2017-10-24 21:58:31 +0800

    update

---
+++ CppLearn/test/housework/10-24/cmake-build-debug/CMakeFiles/10_24.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10_24.dir/main.cpp.o"
+  "10_24.pdb"
+  "10_24"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
