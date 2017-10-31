573c3964e08fc85012a55ac40486cd01ad564e3e
Jonathan Zhang <zhangjianan1996@qq.com>
2017-11-01 01:20:58 +0800

    update

--- CppLearn/test/housework/10-24/main.cpp
+++ CppLearn/test/housework/10-24/main.cpp
@@ -7,6 +7,7 @@ int main()
 {
 
     int i = 0;
+    int stu_n,num;
     SysScore init;
     string text_name = "student.txt";
     init.init_table(text_name);//载入表格
@@ -15,20 +16,19 @@ int main()
         init.quick(1);
         switch (n)
         {
-            case 1:
+            case 1://表格初始化（显示表格)
                 init.Display();
                 break;
-            case 2:
+            case 2://新增学生信息
                 init.init_input();
                 break;
-            case 3:
+            case 3://查找学生信息
                 init.quick(1);
-                int stu_n;
                 cout << "请输入你想查询的学生学号:";
                 cin >> stu_n;
                 init.Find(stu_n);
                 break;
-            case 4:
+            case 4://排序学生信息
                 cout << "1.以学号为排序方式；2.以总成绩为排序方式"<<endl;
                 int ql;
                 cin >> ql;
@@ -38,7 +38,19 @@ int main()
                     init.Display();
                 }
                 break;
-            case 5:
+            case 5://学生数据修改
+                init.quick(1);
+                cout << "请输入你想修改的学生学号:";
+                cin >> stu_n;
+                num = init.Find(stu_n);
+                init.Fix_Student_One(num);
+                break;
+            case 6://导入表格
+                cout << "请输入想要导入的表格";
+                cin >> text_name;
+                init.init_table(text_name);
+                break;
+            case 7://导出表格
                 cout << "1.以学号为排序方式；2.以总成绩为排序方式"<<endl;
                 int ls;
                 cin >> ls;
@@ -56,7 +68,7 @@ int main()
                 cout << "输入错误";
                 break;
         }
-        cout << "是否继续操作,输入0继续，输入其他数字跳出程序";
+        cout << "是否还需要继续其他操作,输入0继续，输入其他数字跳出程序";
         cin >> i;
     }
 }
--- CppLearn/test/housework/10-24/score.h
+++ CppLearn/test/housework/10-24/score.h
@@ -45,8 +45,9 @@ public:
     void quick(int type);
     void init_input();//信息输入到数组
     void init_output();//输出到相应文件
-    void Find(int find_no);//二分查找
+    int Find(int find_no);//二分查找
     void Fix_Student_One(int n);//修改学生信息
+    void Fix_Student_One_detail(int n);
     void quickSort(int,int,int);//快速排序
     float get_all(float a, float b, float c);//得到总分
     float get_average(float all);//得到平均分
@@ -149,7 +150,7 @@ int SysScore :: getTime()
     return  time;
 }
 
-void SysScore::Find(int find_no)
+int SysScore::Find(int find_no)
 {
     int times=0;
     if (scoreList.size() == 0)  //表为空
@@ -172,28 +173,74 @@ void SysScore::Find(int find_no)
                  <<"|计算机导论:"<<scoreList[Mid].computer_score
                  <<"|总分:"<<scoreList[Mid].all_score
                  <<"|平均成绩:"<<scoreList[Mid].avg_score<<endl;
-
-            times = 1;
+            return Mid;
         }
-        if (find_no < scoreList[Mid].stu_no)   //小于中间元素，查找前半部
+        else if (find_no < scoreList[Mid].stu_no)//小于中间元素，查找前半部
         {
             High = Mid - 1;
         }
-        else                              //大于中间元素，查找后半部。
+        else//大于中间元素，查找后半部。
         {
             Low = Mid + 1;
         }
     }
-    if(times==0)//没有找到。
-    {
-        cout << "没有"<< find_no <<"学生数据" << endl;
-    }
+
+    cout << "没有"<< find_no <<"学生数据" << endl;
     cout<<endl;
 }
 
 void SysScore ::Fix_Student_One(int n)
 {
+    int i,j=0;
+    while (j==0)
+    {
+        cout << "你想要修改哪一项？" << endl;
+        cout << "1.学号|2.姓名|3.高等数学|4.大学英语|5.计算机导论" << endl;
+        cout << "请输入选项:";
+        cin >> i;
+        cout << endl << "请输入你要更改的信息" << endl;
+        switch (i)
+        {
+            case 1:
+                cin >> scoreList[n].stu_no;
+                Fix_Student_One_detail(n);
+                break;
+            case 2:
+                cin >> scoreList[n].stu_name;
+                Fix_Student_One_detail(n);
+                break;
+            case 3:
+                cin >> scoreList[n].highMath_score;
+                Fix_Student_One_detail(n);
+                break;
+            case 4:
+                cin >> scoreList[n].english_score;
+                Fix_Student_One_detail(n);
+                break;
+            case 5:
+                cin >> scoreList[n].computer_score;
+                Fix_Student_One_detail(n);
+                break;
+            default:
+                cout << "无修改" << endl;
+                break;
+        }
+        cout << "是否还需要修改，如需修改请输入0，输入其他数退出修改" << endl;
+        cin >> j;
+    }
+}
 
+void SysScore ::Fix_Student_One_detail(int n)
+{
+    scoreList[n].all_score = get_all(scoreList[n].highMath_score,scoreList[n].english_score,scoreList[n].computer_score);
+    scoreList[n].avg_score = get_average(scoreList[n].all_score);
+    cout << "学号："<<scoreList[n].stu_no
+         <<"|姓名:"<<scoreList[n].stu_name
+         <<"|高等数学:"<<scoreList[n].highMath_score
+         <<"|大学英语:"<<scoreList[n].english_score
+         <<"|计算机导论:"<<scoreList[n].computer_score
+         <<"|总分:"<<  scoreList[n].all_score
+         <<"|平均成绩:"<<scoreList[n].avg_score<<endl;
 }
 
 int SysScore :: SystemInit()
@@ -226,15 +273,24 @@ void SysScore ::init_table(string txt_name)
         vector<string> s;
         string pat="|";
         s=split(str,pat);
-        n->stu_no = atoi(s[0].c_str());
-        n->stu_name =s[1];
-        n->highMath_score=atoi(s[2].c_str());
-        n->english_score=atoi(s[3].c_str());
-        n->computer_score=atoi(s[4].c_str());
-        n->all_score = get_all(n->highMath_score,n->english_score,n->computer_score);
-        n->avg_score = get_average(n->all_score);
-        scoreList.push_back(*n);
+        vector<string>::iterator it;
+        char value[] = "";
+        it = find(s.begin(),s.end(),value);
+        if(it!=s.end())
+        {
+            return;
+        }else{
+            n->stu_no = atoi(s[0].c_str());
+            n->stu_name =s[1];
+            n->highMath_score=atoi(s[2].c_str());
+            n->english_score=atoi(s[3].c_str());
+            n->computer_score=atoi(s[4].c_str());
+            n->all_score = get_all(n->highMath_score,n->english_score,n->computer_score);
+            n->avg_score = get_average(n->all_score);
+            scoreList.push_back(*n);
+        }
     }
+    free(n);
     cout << "初始化表格完毕" <<endl;
 }
 
@@ -278,7 +334,7 @@ void SysScore::init_input()
         cin >> s->highMath_score;
         cout << "请输入英语成绩:";
         cin >> s->english_score;
-        cout << "请输入计算机成绩";
+        cout << "请输入计算机成绩:";
         cin >> s->computer_score;
         s->all_score = get_all(s->highMath_score, s->english_score, s->computer_score);
         s->avg_score= get_average(s->all_score);
