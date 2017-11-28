7ccbf14cbe50f4d509008c3ab842e4aca073bfbf
Jonathan Zhang <zhangjianan1996@qq.com>
2017-11-28 23:20:19 +0800

    update

---
+++ CppLearn/test/housework/11-28_1/cmake-build-debug/CMakeFiles/11_28_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11_28_1.dir/main.cpp.obj"
+  "11_28_1.pdb"
+  "11_28_1.exe"
+  "11_28_1.exe.manifest"
+  "lib11_28_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_28_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
